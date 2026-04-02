84bb6755d82d5f71cb35dff29f69e8cee1b9e491
Timothy <timothysuldayan1@gmail.com>
2026-04-02 15:50:55 -0700

    rennamed from Event to EventConfirmation

--- my-tailwind-app/src/App.js
+++ my-tailwind-app/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Event from "./pages/Event";
+import Event from "./pages/Event";import EventPage from "./pages/EventPage";
+
 function App() {
   return (
     <Router>
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/event/:eventId" element={<Event />} />
+        <Route path="/event/:id" element={<EventPage />} />
       </Routes>
     </Router>
   );
